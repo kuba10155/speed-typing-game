f5f2945832087f21cc95a94df1c247dc8b9c8a7a
kuba10155 <j.zborowski24@gmail.com>
2023-02-15 05:58:49 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,6 @@ import React from "react"
 
 export default function App() {
   const {text, timeRemaining, isTimeRunning, wordCount, handleChange, startGame, textBoxRef} = useHook()
-  const STARTING_TIME = 5
-
-  const [text, setText] = React.useState("")
-  const [timeRemaining, setTimeRemaining] = React.useState(STARTING_TIME)
-  const [isTimeRunning, setIsTimeRunning] = React.useState(false)
-  const [wordCount, setWordCount] = React.useState(0)
-
-  const inputRef = React.useRef(null)
-
-  function handleChange(event) {
-    setText(event.target.value)
-  }
-
-  React.useEffect(() => {
-    if(isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-          setTimeRemaining(prevTime => prevTime - 1)
-      }, 1000)
-    } else if(timeRemaining === 0 ) {
-        endGame()
-    }
-  }, [timeRemaining, isTimeRunning])
-
-  function calculateWords(text) {
-    const wordsArr = text.trim().split(" ") // trim usuwa białe spacje przed i za słowem
-    return wordsArr.filter(word => word !== "").length
-  }
-
-  function startGame() {
-    setText("")
-    setTimeRemaining(STARTING_TIME)
-    setIsTimeRunning(true)
-    setWordCount(0)
-    inputRef.current.disabled = false
-    inputRef.current.focus()
-  }
-
-  function endGame() {
-    setIsTimeRunning(false)
-    setWordCount(calculateWords(text))
-  }
 
   return (
     <div>
